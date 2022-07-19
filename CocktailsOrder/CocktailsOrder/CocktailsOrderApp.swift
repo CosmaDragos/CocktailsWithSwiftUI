3d21f9d7124f56c7ecc8819fcbadb47cf4ac715a
Dragos Cosma <cosma.dragos95@gmail.com>
2022-07-19 21:14:03 +0300

    First push

---
+++ CocktailsOrder/CocktailsOrder/CocktailsOrderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CocktailsOrderApp.swift
+//  CocktailsOrder
+//
+//  Created by Cosma Dragos on 19.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct CocktailsOrderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
